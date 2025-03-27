a6be0150ef8cbb485c56a1b73f499e10eef503aa
mnayeembasha <mnayeembasha@gmail.com>
2025-03-27 12:24:19 +0530

    fixed session issues

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       secure: NODE_ENV === "production",
-      sameSite: "lax",
+      // sameSite: "lax",
     },
   })
 );
